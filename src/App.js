7153f7f3d8a27bd909aa85935d53e5cca716a3e4
launchpad5682 <launchpad5682@gmail.com>
2021-07-12 19:26:11 +0530

    Geolocation support added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import { GoButton } from "./components/button.js";
 import { TempCard } from "./components/card.js";
-import { SearchBar } from "./components/search.js";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { fetchWeatherDataByCoordinates } from "./helper/fetchWeatherData.js";
-import { getLocation } from "./helper/geoLocation";
-import { theme } from "./theme/theme.js";
+import { theme, useStyles } from "./theme/theme.js";
 import { Footer } from "./components/footer.js";
+import { TempForm } from "./components/form.js";
+import { useState, useEffect } from "react";
+import { getLocation } from "./helper/geoLocation.js";
+import { fetchWeatherDataByCoordinates } from "./helper/fetchWeatherData.js";
+
+/*
+Creating the state in the parent component and passing 
+it down the build tree
+*/
 
 function App() {
+  const classes = useStyles();
   //let lat,
   //long = getLocation(fetchWeatherDataByCoordinates);
+  //const [tempData, setTempData] = useContext(TempContext);
+  const [tempData, setTempData] = useState({
+    temp: null,
+    city: null,
+    country: null,
+  });
+  const [coordinates, setCoordinates] = useState();
+  // const { latitude, longitude } = getLocation();
+  // console.log(latitude, longitude);
+
+  var options = {
+    enableHighAccuracy: true,
+  };
+
+  function success(pos) {
+    var crd = pos.coords;
+
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+
+    setCoordinates({ latitude: crd.latitude, longitude: crd.longitude });
+    //fetchWeatherDataByCoordinates(coordinates.latitude, coordinates.longitude);
+    //return { latitude: crd.latitude, longitude: crd.longitude }
+  }
+
+  function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  const getGeoLocation = () => {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  };
+
+  useEffect(() => {
+    const obj = getGeoLocation();
+    console.log(obj);
+    // setCoordinates({})
+  }, []);
+
+  if (coordinates) {
+    console.log(coordinates);
+  }
 
   return (
     <ThemeProvider theme={theme}>
-      <div >
-        <SearchBar />
-        <GoButton />
-        <TempCard />
-        <Footer />
+      <div className={classes.root}>
+        <TempForm temp={tempData} setTemp={setTempData} />
+        <TempCard temp={tempData} />
       </div>
+      <Footer className={classes.footer} />
     </ThemeProvider>
   );
 }
