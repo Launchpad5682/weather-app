8be1040d3890cdc3f210f37ddfc12da58bcffd83
launchpad5682 <launchpad5682@gmail.com>
2021-07-14 14:21:00 +0530

    Added progress indicator

--- src/components/card.js
+++ src/components/card.js
@@ -3,21 +3,42 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { TempContext } from "../context/tempContext";
+import { Box } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
-    background: "yellow",
-    color: "green",
-    height: "300px",
-    width: "200px",
+    background: `linear-gradient(45deg, ${theme.palette.secondary.main} 30%, ${theme.palette.secondary.accent} 70%)`,
+    color: theme.palette.primary.textColor,
+    height: "400px",
+    width: "250px",
     margin: "100px auto",
+    borderRadius: "5%",
   },
-});
+}));
 
 export function TempCard() {
   const { tempData, setTempData } = useContext(TempContext);
   const classes = useStyles();
+
+  if (tempData.loading === true) {
+    return (
+      <Card className={classes.root}>
+        <CardContent>
+          <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height="100%"
+          >
+            <CircularProgress />
+            <Typography>Enter a valid City</Typography>
+          </Box>
+        </CardContent>
+      </Card>
+    );
+  }
   return (
     <Card className={classes.root}>
       <CardContent>
