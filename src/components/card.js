104feec509ebbda651e8ff4c1328255893e61056
launchpad5682 <launchpad5682@gmail.com>
2021-07-14 18:07:07 +0530

    Card updated with final UI touches

--- src/components/card.js
+++ src/components/card.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { TempContext } from "../context/tempContext";
 import { Box } from "@material-ui/core";
@@ -11,10 +12,14 @@ const useStyles = makeStyles((theme) => ({
   root: {
     background: `linear-gradient(45deg, ${theme.palette.secondary.main} 30%, ${theme.palette.secondary.accent} 70%)`,
     color: theme.palette.primary.textColor,
+    display: "flex",
     height: "400px",
     width: "250px",
     margin: "100px auto",
     borderRadius: "5%",
+  },
+  title: {
+    color: theme.palette.primary.textColor,
   },
 }));
 
@@ -33,7 +38,7 @@ export function TempCard() {
             height="100%"
           >
             <CircularProgress />
-            <Typography>Enter a valid City</Typography>
+            <Typography variant="h5">Enter a valid City</Typography>
           </Box>
         </CardContent>
       </Card>
@@ -42,15 +47,18 @@ export function TempCard() {
   return (
     <Card className={classes.root}>
       <CardContent>
-        <Typography
-          className={classes.title}
-          color="textSecondary"
-          gutterBottom
-        >
+        <img
+          src={`https://openweathermap.org/img/wn/${tempData.icon}@2x.png`}
+        />
+        <Typography className={classes.title} variant="h2">
           {tempData.temp}&#8451;
         </Typography>
-        <Typography variant="h5" component="h2">
+        <Typography variant="h7">{tempData.weather}</Typography>
+        <Typography variant="h5">
           {tempData.city}, {tempData.country}
+        </Typography>
+        <Typography variant="h7">
+          Feels like {tempData.feels_like}&#8451;
         </Typography>
       </CardContent>
     </Card>
